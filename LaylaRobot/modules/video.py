66385c95ff1d9f19db25fc8bbc915f216655a14e
RSR Official <80620448+RSR-TG-Info@users.noreply.github.com>
2022-02-27 09:32:15 +0530

    Update video.py

--- LaylaRobot/modules/video.py
+++ LaylaRobot/modules/video.py
@@ -4,7 +4,7 @@ import os
 import time
 
 from telethon.tl.types import DocumentAttributeAudio
-from youtube_dl import YoutubeDL
+from yt_dlp import YoutubeDL
 from youtube_dl.utils import (
     ContentTooShortError,
     DownloadError,
